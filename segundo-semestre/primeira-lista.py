af5e827a0a20479e491cabeb0b6d0cf12fd382e7
Erick Wellington <160539840+erickwelli@users.noreply.github.com>
2025-09-19 20:50:05 +0000

    fazendo primeira atividade

---
+++ segundo-semestre/primeira-lista.py
@@ -0,0 +1,23 @@
+#Menores de 16 anos: não pode votar
+#De 16 a 17 ou mais de 70 anos: voto opcional
+#De 18 a 70 anos (inclusive): voto obrigatório
+
+#idade = 16
+
+#if idade < 16:
+#    print("Você não tem idade para votar ainda")
+
+#elif idade == 16 or idade == 17 or idade > 70:
+#    print("Você tem idade para votar, mas é opcional")
+
+#else:
+ #   print("Você tem idade para votar obrigatoriamente")
+
+#questao 2
+
+valor = int(input("Digite o valor que vc deseja: "))
+
+for i in range(0, 11, 2):
+   print(f"{valor} x {i} = {valor*i}")
+
+#questao 3 
